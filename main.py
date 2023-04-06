0f8dd31a1dd8bf07641185187fea0734da50d56c
kk66666666 <115142452+kk66666666@users.noreply.github.com>
2023-04-06 16:46:46 +0800

    更新 pxt.json, main.blocks, main.ts, main.py

---
+++ main.py
@@ -0,0 +1,55 @@
+number = 0
+
+def on_gesture_shake():
+    global number
+    basic.clear_screen()
+    number = randint(1, 6)
+    if number == 1:
+        basic.show_leds("""
+            . . . . .
+                        . . . . .
+                        . . # . .
+                        . . . . .
+                        . . . . .
+        """)
+    elif number == 2:
+        basic.show_leds("""
+            . . . . .
+                        . . . . .
+                        # . . . #
+                        . . . . .
+                        . . . . .
+        """)
+    elif number == 3:
+        basic.show_leds("""
+            . . . . #
+                        . . . . .
+                        . . # . .
+                        . . . . .
+                        # . . . .
+        """)
+    elif number == 4:
+        basic.show_leds("""
+            # . . . #
+                        . . . . .
+                        . . . . .
+                        . . . . .
+                        # . . . #
+        """)
+    elif number == 5:
+        basic.show_leds("""
+            # . . . #
+                        . . . . .
+                        . . # . .
+                        . . . . .
+                        # . . . #
+        """)
+    else:
+        basic.show_leds("""
+            # . . . #
+                        . . . . .
+                        # . . . #
+                        . . . . .
+                        # . . . #
+        """)
+input.on_gesture(Gesture.SHAKE, on_gesture_shake)
--- main.ts
+++ main.ts
@@ -1,54 +1,74 @@
-let number = 0
 input.onGesture(Gesture.Shake, function () {
-    basic.clearScreen()
-    number = randint(1, 6)
-    if (number == 1) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . # . .
-            . . . . .
-            . . . . .
-            `)
-    } else if (number == 2) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            # . . . #
-            . . . . .
-            . . . . .
-            `)
-    } else if (number == 3) {
-        basic.showLeds(`
-            . . . . #
-            . . . . .
-            . . # . .
-            . . . . .
-            # . . . .
-            `)
-    } else if (number == 4) {
-        basic.showLeds(`
-            # . . . #
-            . . . . .
-            . . . . .
-            . . . . .
-            # . . . #
-            `)
-    } else if (number == 5) {
-        basic.showLeds(`
-            # . . . #
-            . . . . .
-            . . # . .
-            . . . . .
-            # . . . #
-            `)
+    if (ok != 2) {
+        basic.clearScreen()
+        number = randint(1, 6)
+        if (number == 1) {
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . # . .
+                . . . . .
+                . . . . .
+                `)
+            sum += 1
+        } else if (number == 2) {
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                # . . . #
+                . . . . .
+                . . . . .
+                `)
+            sum += 2
+        } else if (number == 3) {
+            basic.showLeds(`
+                . . . . #
+                . . . . .
+                . . # . .
+                . . . . .
+                # . . . .
+                `)
+            sum += 3
+        } else if (number == 4) {
+            basic.showLeds(`
+                # . . . #
+                . . . . .
+                . . . . .
+                . . . . .
+                # . . . #
+                `)
+            sum += 4
+        } else if (number == 5) {
+            basic.showLeds(`
+                # . . . #
+                . . . . .
+                . . # . .
+                . . . . .
+                # . . . #
+                `)
+            sum += 5
+        } else {
+            basic.showLeds(`
+                # . . . #
+                . . . . .
+                # . . . #
+                . . . . .
+                # . . . #
+                `)
+            sum += 6
+        }
+        ok += 1
     } else {
-        basic.showLeds(`
-            # . . . #
-            . . . . .
-            # . . . #
-            . . . . .
-            # . . . #
-            `)
+        if (sum > 9) {
+            basic.showString("Win")
+        } else {
+            basic.showString("Lose")
+        }
+        ok = 0
     }
 })
+let number = 0
+let sum = 0
+let ok = 0
+ok = 0
+sum = 0
